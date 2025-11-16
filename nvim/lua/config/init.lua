5c00c6cdabe0a365da47a255fb3cc30efded58f9
Duane Groves <duane.s.groves@gmail.com>
2025-11-15 19:42:18 -0600

    feat(nvim): added descriptions to LspAttach keymaps

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -9,24 +9,49 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     -- Set buffer-local keybindings
     local opts = { buffer = args.buf }
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)       -- Go to definition
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)      -- Go to declaration
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)   -- Go to implementation
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)       -- List references
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)             -- Hover info
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts) -- Signature help
+
+    -- LSP navigation
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, {
+      desc = "Go to definition"
+    }))
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, vim.tbl_extend("force", opts, {
+      desc = "Go to declaration"
+    }))
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, vim.tbl_extend("force", opts, {
+      desc = "Go to implementation"
+    }))
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, vim.tbl_extend("force", opts, {
+      desc = "List references"
+    }))
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, {
+      desc = "Hover documentation"
+    }))
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, vim.tbl_extend("force", opts, {
+      desc = "Show signature help"
+    }))
 
     -- LSP actions
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)  -- Rename symbol
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts) -- Code actions
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, {
+      desc = "Rename symbol"
+    }))
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, {
+      desc = "Code actions"
+    }))
 
     -- Diagnostics navigation
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-    vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts) -- Show diagnostics in floating window
-    vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts) -- Set diagnostics in location list
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, vim.tbl_extend("force", opts, {
+      desc = "Go to previous diagnostic"
+    }))
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, vim.tbl_extend("force", opts, {
+      desc = "Go to next diagnostic"
+    }))
+    vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, vim.tbl_extend("force", opts, {
+      desc = "Show diagnostic in floating window"
+    }))
+    vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, vim.tbl_extend("force", opts, {
+      desc = "Populate location list with diagnostics"
+    }))
   end,
 })
-
 
 vim.diagnostic.config({ virtual_text = true })
